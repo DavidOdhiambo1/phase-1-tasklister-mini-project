7ceb2a5f4b52089c2ba2a0b683129ac55e4428e0
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-16 21:31:08 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form")
   form.addEventListener("submit", (e)=> {
     e.preventDefault()
-    console.log(e.target.new)
+    handleTask(e.target.new.value)
+    form.reset()
   })
+function handleTask (task){
+  const ul = document.querySelector('#tasks')
+  const li = document.createElement('li')
+  li.textContent = task
+  ul.appendChild(li)
+  const deleteBtn = document.createElement('button')
+  deleteBtn.textContent= "Delete"
+  deleteBtn.style.backgroundColor = 'red'
+  li.append(deleteBtn)
+  deleteBtn.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e){
+  console.log(e)
+  e.target.parentNode.remove()
+}
+
+
+
 });
