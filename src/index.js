2add8863a02dc303fc4aa7ee737391f7d388a032
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-16 18:27:22 +0300

    deliverables achieved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    console.log(e.target.new)
+  })
 });
